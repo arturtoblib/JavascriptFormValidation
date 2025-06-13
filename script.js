a5f70cab8968caa1dd8c960b27bcfd1684a5591d
arturtoblib <toblib.artur@gmail.com>
2025-06-13 15:49:21 -0300

    segundo commit

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
-console.log("Página criada por Artur Toblib\nO intuito dessa página é estudar mais sobre validações de form com Javascript")
+console.log(
+  "Página criada por Artur Toblib\nO intuito dessa página é estudar mais sobre validações de form com Javascript"
+);
 
 const nome = document.querySelector("#nome");
 const senha = document.querySelector("#senha");
 const form = document.querySelector("form");
 
 form.addEventListener("submit", (e) => {
-        let enviarForm = true;
+  let enviarForm = true;
 
-    if (!nome.value) {
-        console.log("Nome está vazio");
-        enviarForm = false;
-    }
+  if (!nome.value) {
+    console.log("Nome está vazio");
+    enviarForm = false;
+  }
 
-        if (!senha.value) {
-        console.log("Senha está vazia");
-        enviarForm = false;
-    }
+  if (!senha.value) {
+    console.log("Senha está vazia");
+    enviarForm = false;
+  }
 
-    if (!enviarForm){
+  if (senha.value.length > 10 || senha.value.length < 6) {
+    console.log("A senha é muito longa.");
+    enviarForm = false;
+  }
+
+  if (!enviarForm) {
     e.preventDefault();
-    }
-
-
-
+  }
 });
-
-
